7f27957eff01ff09b5033d70496ed499efc28d40
huangmingxian <ihuangmx@icloud.com>
2024-09-29 17:00:49 +0800

    basics-of-ai-python-coding

--- 2024-09-28-chatgpt-building-system/L006-use-tool.py
+++
@@ -1,8 +0,0 @@
-from tool import get_completion_from_messages
-
-messages=[
-    {"role": "system", "content": "当我请求帮助写一些东西时，你会回复一份文档，其中每段至少包含一个笑话或有趣的评论。"},
-    {"role": "user", "content": "给我的螺纹钢供应商写一封感谢信，感谢他们在短时间内准时交货。这使我们能够交付一份重要的订单。"}
-]
-
-print(get_completion_from_messages(messages))
---
+++ 2024-09-29-ai-python-for-beginners/basics-of-ai-python-coding/use-llm.py
@@ -0,0 +1,3 @@
+from helper_functions import print_llm_response
+
+print_llm_response("你好，Python")
